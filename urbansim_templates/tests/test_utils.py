786aa5fa13a320c32cf692ef8ea6eca66717de57
Sam Maurer <maurer@berkeley.edu>
2018-08-30 14:04:56 -0700

    Version comparison function

--- urbansim_templates/tests/test_utils.py
+++ urbansim_templates/tests/test_utils.py
@@ -5,6 +5,17 @@ from urbansim_templates import utils
 
 def test_version_parse():
     assert utils.version_parse('0.1.0.dev0') == (0, 1, 0, 0)
-    assert utils.version_parse('0.115.3') == (0, 115, 3, 0)
+    assert utils.version_parse('0.115.3') == (0, 115, 3, None)
     assert utils.version_parse('3.1.dev7') == (3, 1, 0, 7)
-    assert utils.version_parse('5.4') == (5, 4, 0, 0)
+    assert utils.version_parse('5.4') == (5, 4, 0, None)
+
+def test_version_greater_or_equal():
+    assert utils.version_greater_or_equal('2.0', '0.1.1') == True    
+    assert utils.version_greater_or_equal('0.1.1', '2.0') == False    
+    assert utils.version_greater_or_equal('2.1', '2.0.1') == True
+    assert utils.version_greater_or_equal('2.0.1', '2.1') == False
+    assert utils.version_greater_or_equal('1.1.3', '1.1.2') == True
+    assert utils.version_greater_or_equal('1.1.2', '1.1.3') == False
+    assert utils.version_greater_or_equal('1.1.3', '1.1.3') == True
+    assert utils.version_greater_or_equal('1.1.3.dev1', '1.1.3.dev0') == True
+    assert utils.version_greater_or_equal('1.1.3.dev0', '1.1.3') == False
--- urbansim_templates/utils.py
+++ urbansim_templates/utils.py
@@ -5,7 +5,7 @@ def version_parse(v):
     """
     Parses a version string into its component parts. String is expected to follow the 
     pattern "0.1.1.dev0", which would be parsed into (0, 1, 1, 0). The first two 
-    components are required, and the latter two are assumed to be zero if missing.
+    components are required. The third is set to 0 if missing, and the fourth to None.
     
     Parameters
     ----------
@@ -14,10 +14,10 @@ def version_parse(v):
     
     Returns
     -------
-    tuple of four integers
+    tuple with format (int, int, int, int or None)
     
     """
-    v4 = 0
+    v4 = None
     if 'dev' in v:
         v4 = int(v.split('dev')[1])
         v = v.split('dev')[0]  # 0.1.dev0 -> 0.1.
@@ -41,26 +41,46 @@ def version_parse(v):
 def version_greater_or_equal(a, b):
     """
     Tests whether version string 'a' is greater than or equal to version string 'b'.
-    Version syntax is expected to follow this pattern: 0.1.dev0 -> 0.1.dev1 -> 0.1 ->
-    0.1.1.dev0 -> 0.1.1 -> 0.2 -> 1.0, etc.
+    Version syntax should follow the pattern described for `version_parse()`. 
     
+    Note that 'dev' versions are pre-releases, so '0.2' < '0.2.1.dev5' < '0.2.1'.
+        
     Parameters
     ----------
     a : str
-        First version string, should be formatted as above. Required.
+        First version string, formatted as described in `version_parse()`. 
     b : str
-        Second version string, should be formatted as above. Required.
+        Second version string, formatted as described in `version_parse()`. 
     
     Returns
     -------
-    bool
+    boolean
     
     """
+    a = version_parse(a)
+    b = version_parse(b)
     
+    if (a[0] > b[0]):
+        return True
     
+    elif (a[0] == b[0]):
+        if (a[1] > b[1]):
+            return True
+            
+        elif (a[1] == b[1]):
+            if (a[2] > b[2]):
+                return True
+            
+            elif (a[2] == b[2]):
+                if (a[3] == None):
+                    return True
+                
+                elif (b[3] == None):
+                    return False
+                
+                elif (a[3] >= b[3]):
+                    return True
     
-    # Parse each of the strings into four components: x.x.(x).dev(x), where the first two
-    # values are always present and the latter two are assumed to be zero if missing, 
-    # and compare each in turn
+    return False
     
     
