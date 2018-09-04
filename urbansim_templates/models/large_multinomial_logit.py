d5d4b924b40f4cee8b9012dc82f0bfde391a88c5
Sam Maurer <maurer@berkeley.edu>
2018-09-04 12:18:32 -0700

    Updating version numbers

--- urbansim_templates/models/large_multinomial_logit.py
+++ urbansim_templates/models/large_multinomial_logit.py
@@ -14,7 +14,7 @@ from .shared import TemplateStep
 from .. import modelmanager as mm
 
 
-TEMPLATE_VERSION = '0.1dev2'
+TEMPLATE_VERSION = '0.1dev3'
 
 class LargeMultinomialLogitStep(TemplateStep):
     """
