fdde25d6fc774cb89d2f2f03117d90f66f4ca6e7
Gitiauxx <xavier.gitiaux@colorado.edu>
2018-09-24 13:47:48 -0400

    Add test for random forest

---
+++ urbansim_templates/tests/test_random_forest.py
@@ -0,0 +1,39 @@
+import orca
+import numpy as np
+import pandas as pd
+import pytest
+
+from urbansim_templates import modelmanager
+from urbansim_templates.models.regression import RandomForestRegressionStep
+
+d1 = {'a': np.random.random(100),
+      'b': np.random.randint(2, size=100),
+	  'c': np.random.normal(size=100)}
+
+obs = pd.DataFrame(d1)
+orca.add_table('obs', obs)
+
+
+def test_rf():
+    """
+    For now this just tests that the code runs.
+    
+    """
+    modelmanager.initialize()
+
+    m = RandomForestRegressionStep()
+    m.tables = 'obs'
+    m.model_expression = 'b ~ a + c'
+    
+    m.fit()
+    
+    m.name = 'random_forest-test'
+    modelmanager.register(m)
+    
+    #modelmanager.initialize()
+    #m = modelmanager.get_step('random_forest-test')
+    
+	
+if __name__ == '__main__':
+    test_rf()
+	
