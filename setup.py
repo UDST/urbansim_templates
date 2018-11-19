1b92fc2760eb6ff8f337f1ad11169eb6221a0dc4
Sam Maurer <maurer@berkeley.edu>
2018-11-19 09:33:17 -0800

    Revert "Revert "[0.1.dev18] Installation improvements and continuous integration""

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+requirements = [item.strip() for item in requirements]
+
 setup(
     name='urbansim_templates',
     version='0.1.dev17',
@@ -13,14 +17,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: BSD License'
     ],
     packages=find_packages(exclude=['*.tests']),
-    install_requires=[
-        'numpy >= 1.14',
-        'orca >= 1.4',
-        'pandana >= 0.3',
-        'pandas >= 0.22',
-        'statsmodels >= 0.8',
-        'urbansim >= 3.1.1'
-    ]
+    install_requires=requirements
 )
