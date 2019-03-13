952c1f3d03b90da92bafdaba4e5814361c865787
Sam Maurer <maurer@berkeley.edu>
2019-03-13 12:03:11 -0700

    More tests

--- tests/test_column_expression.py
+++ tests/test_column_expression.py
@@ -18,9 +18,9 @@ def orca_session():
     orca.clear_all()
     modelmanager.initialize()
 
-    d1 = {'id': np.arange(10), 
-          'a': np.random.random(10),
-          'b': np.random.choice(np.arange(20), size=10)}
+    d1 = {'id': np.arange(5), 
+          'a': np.random.random(5),
+          'b': np.random.choice(np.arange(20), size=5)}
 
     df = pd.DataFrame(d1).set_index('id')
     orca.add_table('obs', df)
@@ -34,17 +34,63 @@ def test_template_validity():
     assert validate_template(ColumnFromExpression)
 
 
-def test_run_requirements(orca_session):
+def test_missing_colname(orca_session):
     """
-    
+    Missing column_name should raise a ValueError.
     
     """
-    pass
+    c = ColumnFromExpression()
+    c.table = 'tab'
+    c.expression = 'a'
+    
+    try:
+        c.run()
+    except ValueError as e:
+        print(e)
+        return
+    
+    pytest.fail()
+
+
+def test_missing_table(orca_session):
+    """
+    Missing table should raise a ValueError.
+    
+    """
+    c = ColumnFromExpression()
+    c.column_name = 'col'
+    c.expression = 'a'
+    
+    try:
+        c.run()
+    except ValueError as e:
+        print(e)
+        return
+    
+    pytest.fail()
+
+
+def test_missing_expression(orca_session):
+    """
+    Missing expression should raise a ValueError.
+    
+    """
+    c = ColumnFromExpression()
+    c.column_name = 'col'
+    c.table = 'tab'
+    
+    try:
+        c.run()
+    except ValueError as e:
+        print(e)
+        return
+    
+    pytest.fail()
 
 
 def test_expression(orca_session):
     """
-    Check that column is created correctly.
+    Check that column is created and expression evaluated correctly.
     
     """
     c = ColumnFromExpression()
@@ -53,14 +99,57 @@ def test_expression(orca_session):
     c.expression = 'a * 5 + sqrt(b)'
     
     c.run()
-    series = orca.get_table('obs').get_column('c')
-    print(series)
+    
+    val1 = orca.get_table('obs').get_column('c')
+    df = orca.get_table('obs').to_frame()
+    val2 = df.a * 5 + np.sqrt(df.b)
+    assert(val1.equals(val2))
     
 
-def test_data_type_and_missing_values(orca_session):
+def test_data_type(orca_session):
     """
+    Check that casting data type works.
+    
     """
-    pass
+    orca.add_table('tab', pd.DataFrame({'a': [0.1, 1.33, 2.4]}))
+    
+    c = ColumnFromExpression()
+    c.column_name = 'b'
+    c.table = 'tab'
+    c.expression = 'a'
+    c.run()
+    
+    v1 = orca.get_table('tab').get_column('b').values
+    np.testing.assert_equal(v1, [0.1, 1.33, 2.4])
+    
+    c.data_type = 'int'
+    c.run()
+
+    v1 = orca.get_table('tab').get_column('b').values
+    np.testing.assert_equal(v1, [0, 1, 2])
+
+
+def test_missing_values(orca_session):
+    """
+    Check that filling in missing values works.
+    
+    """
+    orca.add_table('tab', pd.DataFrame({'a': [0.1, np.nan, 2.4]}))
+    
+    c = ColumnFromExpression()
+    c.column_name = 'b'
+    c.table = 'tab'
+    c.expression = 'a'
+    c.run()
+    
+    v1 = orca.get_table('tab').get_column('b').values
+    np.testing.assert_equal(v1, [0.1, np.nan, 2.4])
+    
+    c.missing_values = 5
+    c.run()
+
+    v1 = orca.get_table('tab').get_column('b').values
+    np.testing.assert_equal(v1, [0.1, 5.0, 2.4])
 
 
 def test_modelmanager_registration(orca_session):
